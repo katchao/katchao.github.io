3369338386e27dc61e22f2c8a038365f0276fcbd
Katherine Chao <me@katherinechao.com>
2023-10-07 20:41:52 -0700

    show nav on toggle click

--- main.js
+++ main.js
@@ -2,9 +2,17 @@
 const addBgToHeaderOnScroll = () => {
     const header = document.getElementById("header");
     if (this.window.scrollY >= 50) {
-        header.classList.add("header-bg").remove("header-transparent");
+        header.classList.add("header-bg");
     } else {
-        header.classList.remove("header-bg").add("header-transparent");
+        header.classList.remove("header-bg");
     }
 };
 window.addEventListener("scroll", addBgToHeaderOnScroll);
+
+/* ------- Toggle menu -------- */
+const nav = document.getElementById("nav");
+const navToggle = document.getElementById("nav-toggle");
+
+navToggle.addEventListener("click", () => {
+    nav.classList.add("show-nav");
+});
