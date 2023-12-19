c9cd3f1fbdef4a7d177a9740c8d72d202ae48eb1
Katherine Chao <me@katherinechao.com>
2023-12-18 20:37:59 -0800

    added background fadeout, adjusted parallax to be slower

--- scripts/main.js
+++ scripts/main.js
@@ -40,7 +40,7 @@ let parallax = new Rellax(".parallax");
 /* ------- Gsap slow reveal -------- */
 gsap.from("#landscape1__first-slope", 1.2, { opacity: 0, y: -200, delay: 0.4 });
 gsap.from("#landscape1__clouds", 1.2, { opacity: 0, y: -200, delay: 0.8 });
-gsap.from("#landscape1__sun", 1.2, { opacity: 0, x: 150, delay: 0.7 });
+gsap.from("#landscape1__sun", 1.2, { opacity: 0, x: 150, delay: 1 });
 gsap.from("#landscape1__second-slope", 1.2, { opacity: 0, y: 150, delay: 0.8 });
 gsap.from("#landscape1__third-slope", 1.2, { opacity: 0, y: -50, delay: 0.4 });
 
